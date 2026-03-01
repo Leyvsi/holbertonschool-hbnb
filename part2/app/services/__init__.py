d3fddf1d374d6975a2dc6d0d4a721a2ce2a40dfe
leyvsi <sylviecosta78500@gmail.com>
2026-03-01 14:14:19 +0100

    Fix circular imports and clean up init files

--- part2/app/services/__init__.py
+++ part2/app/services/__init__.py
@@ -1,11 +1,6 @@
-from flask import Flask
-from flask_restx import Api
-from app.api.v1.users import api as users_ns
+#!/usr/bin/python3
+"""Services initialization"""
+from app.services.facade import HBnBFacade
 
-def create_app():
-    app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
-
-    # Register the users namespace
-    api.add_namespace(users_ns, path='/api/v1/users')
-    return app
+# Create a single instance of the facade to be used everywhere
+facade = HBnBFacade()
