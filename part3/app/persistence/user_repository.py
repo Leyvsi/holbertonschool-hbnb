1e17d98d407dfcdad907f951afa1e3f9f01d3af1
leyvsi <sylviecosta78500@gmail.com>
2026-04-07 16:01:40 +0200

    add user_repository.py n fix facade.py

---
+++ part3/app/persistence/user_repository.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Specific repository for User entity."""
+from app.models.user import User
+from app.persistence.repository import SQLAlchemyRepository
+
+class UserRepository(SQLAlchemyRepository):
+    def __init__(self):
+        """Initializes the repository with the User model."""
+        super().__init__(User)
+
+    def get_user_by_email(self, email):
+        """Retrieves a user by their email address."""
+
+        return self.model.query.filter_by(email=email).first()
